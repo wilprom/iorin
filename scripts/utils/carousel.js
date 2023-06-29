121e0afaf49003e199dfb0a30a2f3c52c185077f
Wilfried Prombo <89302586+WilfriedProm@users.noreply.github.com>
2023-06-29 19:41:58 +0100

    :construction: Add section 'our products' and update heroCarousel

--- scripts/utils/carousel.js
+++ scripts/utils/carousel.js
@@ -6,13 +6,10 @@ const heroSplide = new Splide(splides[0], {
   arrows: false,
   type: 'fade',
   rewind: true,
-  speed: 300,
-  autoplay: true,
-  interval: 7000,
   // Add classes for custom elt
   classes: {
     pagination: 'splide__pagination hero-carousel__dots',
-  }
+  },
 })
 
 heroSplide.mount()
@@ -21,17 +18,37 @@ heroSplide.mount()
 const bestProductSplide = new Splide(splides[1], {
   type: 'loop',
   perPage: 3,
-  gap : '1rem',
+  gap: '1rem',
   focus: 'center',
   pagination: false,
   // Add classes for custom elt
   classes: {
-		arrow : 'splide__arrow best-product-carousel__arrow',
-		prev  : 'splide__arrow--prev best-product-carousel__arrow__prev',
-		next  : 'splide__arrow--next best-product-carousel__arrow__next',
-  }
+    arrow: 'splide__arrow best-product-carousel__arrow',
+    prev: 'splide__arrow--prev best-product-carousel__arrow__prev',
+    next: 'splide__arrow--next best-product-carousel__arrow__next',
+  },
 })
 
 bestProductSplide.on('arrows:mounted')
 
 bestProductSplide.mount()
+
+// Best products splide
+const ourProductsSplide = new Splide(splides[2], {
+  type: 'loop',
+  padding: '2rem',
+  perPage: 4,
+  gap: '1rem',
+  focus: 'center',
+  pagination: false,
+  // Add classes for custom elt
+  classes: {
+    arrow: 'splide__arrow our-products-carousel__arrow',
+    prev: 'splide__arrow--prev our-products-carousel__arrow__prev',
+    next: 'splide__arrow--next our-products-carousel__arrow__next',
+  },
+})
+
+ourProductsSplide.on('arrows:mounted')
+
+ourProductsSplide.mount()
